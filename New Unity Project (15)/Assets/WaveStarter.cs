fb7dc89191a34dd8b0de3dff57c84672cee3cdab
Jonkert <78416240+Jonkert@users.noreply.github.com>
2021-09-23 14:41:52 +0200

    wave spawner fixes

---
+++ New Unity Project (15)/Assets/WaveStarter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveStarter: MonoBehaviour
+{
+	public Transform enemyPrefab;
+
+	public Transform spawnPoint;
+
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
+
+	private int waveIndex = 0;
+
+	void Update (){
+		if (countdown <= 0f){
+			StartCoroutine(SpawnWave());
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+	}
+	IEnumerator SpawnWave ()
+	{
+		
+		for (int i = 0; i < waveNum; i++)
+		{
+			SpawnEnemy ();
+			yield return new WaitForSeconds(0.5f);
+		}
+
+		waveNum++;
+		}
+
+		void SpawnEnemy ()
+		{
+		Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+		}
+}
