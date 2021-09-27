fe85c83482006492de76919e62f53c8e7bace5fe
Jonkert <78416240+Jonkert@users.noreply.github.com>
2021-09-27 13:43:28 +0200

    turret targeting

--- New Unity Project (15)/Assets/Jauke/scripts/WaveSpawner.cs
+++ New Unity Project (15)/Assets/Jauke/scripts/WaveSpawner.cs
@@ -11,6 +11,8 @@ public class WaveSpawner : MonoBehaviour {
 	public float timeBetweenWaves = 5f;
 	private float countdown = 2f;
 
+	public Text waveCountdownText;
+
 	private int waveIndex = 0;
 
 	void Update ()
@@ -22,6 +24,8 @@ public class WaveSpawner : MonoBehaviour {
 		}
 
 		countdown -= Time.deltaTime;
+
+		waveCountdownText.text = Mathf.Round(countdown).ToString();
 	}
 
 	IEnumerator SpawnWave ()
